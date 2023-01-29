d8958a884031f37c5102b9839a8cc6666f1b124d
dmshikder <dmshikder@gmail.com>
2023-01-30 01:38:14 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-
-const express = require('express');
-const app= express();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-app.get('/', (req,res)=>{
-    res.send('power hack backend')
+app.use(cors());
+app.use(express.json());
+
+// mongodb connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9qafziu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const billingList = client.db("power_hack").collection("billing-list");
+
+    app.get("/billing-list", async (req, res) => {
+      const query = {};
+      const cursor = billingList.find(query);
+      const allBills = await cursor.toArray();
+      res.send(allBills)
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("power hack backend");
 });
 
 
-app.listen(port, ()=>{
-    console.log('listening to port', port);
-})
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
